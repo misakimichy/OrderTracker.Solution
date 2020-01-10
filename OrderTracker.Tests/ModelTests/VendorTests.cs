184d97d2bd7e63ba6906805def4edef43b921cba
Misaki Koonce <misaki.koonce@gmail.com>
2020-01-10 10:50:33 -0800

    TDD for Vendor class: List<Vendor> and Id object and GetAll, ClearAll method.

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,18 +1,99 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using OrderTracker.Models;
 
 namespace OrderTracker.Tests
 {
     [TestClass]
-    public class VendorTest
+    public class VendorTest : IDisposable
     {
+
+        public void Dispose()
+        {
+            Vendor.ClearAll();
+        }
+
         [TestMethod]
         public void VendorConstructor_CreateInstanceOfVendor_Vendor()
         {
-            Vendor newVendor = new Vendor("bakery", "We are bakery");
+            Vendor newVendor = new Vendor("bakery", "We locates in Seattle.");
+
             Assert.AreEqual(typeof(Vendor), newVendor.GetType());
         }
- 
+
+        [TestMethod]
+        public void GetName_ReturnVendorName_String()
+        {
+            string name = "Jerry's Bakery";
+            string description = "We locates in Seattle.";
+            Vendor newVendor = new Vendor(name, description);
+            string result = newVendor.Name;
+
+            Assert.AreEqual(name, result);
+        }
+
+        [TestMethod]
+        public void SetName_SetName_String()
+        {
+            string name = "Jerry's Bakery";
+            string description = "We locates in Seattle.";
+            Vendor newVendor = new Vendor(name, description);
+            string updatedName = "Tony's Bakery";
+            newVendor.Name = updatedName;
+            string result = newVendor.Name;
+
+            Assert.AreEqual(updatedName, result);
+        }
+
+        [TestMethod]
+        public void GetDescription_ReturnVendorDescription_String()
+        {
+            string name = "Jerry's Bakery";
+            string description = "We locates in Seattle.";
+            Vendor newVendor = new Vendor(name, description);
+            string result = newVendor.Description;
+
+            Assert.AreEqual(description, result);
+        }
+
+        [TestMethod]
+        public void SetDescription_SetDescription_String()
+        {
+            string name = "Jerry's Bakery";
+            string description = "We locates in Seattle.";
+            Vendor newVendor = new Vendor(name, description);
+            string updatedDescription = "We locates in Bellevue";
+            newVendor.Description = updatedDescription;
+            string result = newVendor.Description;
+
+            Assert.AreEqual(updatedDescription, result);
+        }
+
+        [TestMethod]
+        public void GetAll_ReturnsVendors_VendorList()
+        {
+            string name1 = "Jerry's Bakery";
+            string name2 = "Tony's Bakery";
+            string description1 = "We locates in Seattle.";
+            string description2 = "We locates in Bellevue";
+            Vendor newVendor1 = new Vendor(name1, description1);
+            Vendor newVendor2 = new Vendor(name2, description2);
+            List<Vendor> newList = new List<Vendor> {newVendor1, newVendor2};
+            List<Vendor> result = Vendor.GetAll();
+
+            CollectionAssert.AreEqual(newList, result);
+        }
+
+        [TestMethod]
+        public void GetId_VendorInstantiateWithanId_Int()
+        {
+            string name = "Jerry's Bakery";
+            string description = "We locates in Seattle.";
+            Vendor newVendor = new Vendor(name, description);
+            int result = newVendor.Id;
+
+            Assert.AreEqual(1, result);
+        }
     }
 }
--- OrderTracker/Models/Vendor.cs
+++ OrderTracker/Models/Vendor.cs
@@ -16,7 +16,15 @@ namespace OrderTracker.Models
             _instances.Add(this);
             Id = _instances.Count;
         }
+        
+        public static List<Vendor> GetAll()
+        {
+            return _instances;
+        }
 
-        // Method
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
     }
 }
