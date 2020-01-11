df0e39010e70039cfc5ffd7b2537838afb3a17a1
Misaki Koonce <misaki.koonce@gmail.com>
2020-01-10 16:55:44 -0800

    Add Order view and takes vendor order.

--- VendorTracker/Controllers/OrderController.cs
+++ VendorTracker/Controllers/OrderController.cs
@@ -1,34 +1,28 @@
-// using Microsoft.AspNetCore.Mvc;
-// using System;
-// using System.Collections.Generic;
-// using VendorTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using VendorTracker.Models;
 
-// namespace VendorTracker.Controllers
-// {
-//     public class OrderController : Controller
-//     {
-//         [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
-//         public ActionResult Show(int vendorId, int orderId)
-//         {
-//             Vendor vendor = Vendor.Find(vendorId);
-//             Order order = Order.Find(orderId);
-//             Dictionary<string, object> dict = new Dictionary<string, object>();
-//             dict.Add("order", order);
-//             dict.Add("vendor", vendor);
-//             return View(dict);
-//         }
+namespace VendorTracker.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+        public ActionResult Show(int vendorId, int orderId)
+        {
+            Vendor vendor = Vendor.Find(vendorId);
+            Order order = Order.Find(orderId);
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            dict.Add("order", order);
+            dict.Add("vendor", vendor);
+            return View(dict);
+        }
 
-//         [HttpPost("/vendors/{vendorId/orders")]
-//         public ActionResult Create(int vendorId, string orderTitle, string orderDescription, int orderPrice, DateTime orderDate)
-//         {
-//             Vendor selectedVendor = Vendor.Find(vendorId);
-//             Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
-//             selectedVendor.AddOrder(newOrder);
-//             List<Order> vendorOrder = selectedVendor.Orders;
-//             Dictionary<string, object> dict = new Dictionary<string, object>();
-//             dict.Add("orders", vendorOrder);
-//             dict.Add("vendor", selectedVendor);
-//             return View("Show", dict);
-//         }
-//     }
-// }
+        [HttpGet("/vendors/{vendorId}/orders/new")]
+        public ActionResult New(int vendorId)
+        {
+            Vendor vendor = Vendor.Find(vendorId);
+            return View(vendor);
+        }
+    }
+}
--- VendorTracker/Controllers/VendorController.cs
+++ VendorTracker/Controllers/VendorController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using VendorTracker.Models;
 
@@ -19,13 +20,13 @@ namespace VendorTracker.Controllers
             return View();
         }
 
-        [HttpGet("/vendors/{id}")]
-        public ActionResult Show(int id)
+        [HttpGet("/vendors/{vendorId}")]
+        public ActionResult Show(int vendorId)
         {
-            Vendor selectedVendor = Vendor.Find(id);
-            List<Order> vendorOrder = selectedVendor.Orders;
             // This dictionary has a vendor name and a order object
             Dictionary<string, object> dict = new Dictionary<string, object>();
+            Vendor selectedVendor = Vendor.Find(vendorId);
+            List<Order> vendorOrder = selectedVendor.Orders;
             dict.Add("vendor", selectedVendor);
             dict.Add("orders", vendorOrder);
             return View(dict);
@@ -34,12 +35,21 @@ namespace VendorTracker.Controllers
         [HttpPost("/vendors")]
         public ActionResult Create(string name, string description)
         {
-            if(name == null)
-            {
-                return RedirectToAction("Index");
-            }
             Vendor myVendor = new Vendor(name, description);
             return RedirectToAction("Index");
+        }
+
+        [HttpPost("/vendors/{vendorId}/orders")]
+        public ActionResult Create(int vendorId, string orderTitle, string orderDescription, int orderPrice, DateTime orderDate)
+        {
+            Vendor selectedVendor = Vendor.Find(vendorId);
+            Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+            selectedVendor.AddOrder(newOrder);
+            List<Order> vendorOrders = selectedVendor.Orders;
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            dict.Add("orders", vendorOrders);
+            dict.Add("vendor", selectedVendor);
+            return View("Show", dict);
         }
     }
 }
